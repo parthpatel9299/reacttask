c10c32ddc8f7a985793e1c969eb6f244260c159a
123 <apple@192.168.1.77>
2023-02-03 18:54:56 +0530

    Condition

--- src/components/Game.js
+++ src/components/Game.js
@@ -18,17 +18,15 @@ const Slot = (props) => {
           {X} {Y} {Z}
         </h1>
         <h1>pls Try Again</h1>
-
       </div>
     );
   }
 };
-
 export default function Game() {
   return (
     <>
-      <h1 className="heading_style"> game </h1>
-      <Slot X=" P " Y=" P " Z=" P " />
+      <h1 className="heading_style"> Game </h1>
+      <Slot X=" " Y=" P " Z=" P " />
       <Slot X=" P " Y=" P " Z=" Z " />
       <div className="space"></div>
     </>
